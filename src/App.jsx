00be34a6606ef2346c91a62609ebe48a4d027efe
ankit kumar <87237698+ankit7840@users.noreply.github.com>
2023-12-25 14:00:46 +0000

    // testing with the react // 1. create a react app

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,15 @@
 import './App.css';
+import Second from './second';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
-  );
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <Second />
+    </>
+  )
 }
 
 export default App;
---
+++ src/second.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Second() {
+  return (
+    <h1 className="text-xl font-bold underline">
+         hello from second
+  </h1>
+  )
+}
+
+export default Second
